a86c4fbdad80f86593ac7eb96efdb44d7f06dff6
thisarakaushan <thisarak9992@gmail.com>
2025-08-12 13:56:43 +0530

    Initial commit for stripe-backend

---
+++ index.js
@@ -0,0 +1,25 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    try {
+      const { amount } = req.body;
+
+      if (!amount || amount <= 0) {
+        return res.status(400).json({ error: 'Invalid amount' });
+      }
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        automatic_payment_methods: { enabled: true },
+      });
+
+      res.json({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+};
